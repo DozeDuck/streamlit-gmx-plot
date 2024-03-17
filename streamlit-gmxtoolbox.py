7601f02a6756f5345e78c6281ff94b26aa6a935a
Shangze <89918650+DozeDuck@users.noreply.github.com>
2024-03-17 18:40:07 +0000

    Update streamlit-gmxtoolbox.py

--- streamlit-gmxtoolbox.py
+++ streamlit-gmxtoolbox.py
@@ -110,11 +110,10 @@ class plotly_go():
         elif file1.endswith(".csv"):
             found = False
             for key in flags_map:
-                if key.strip(',') in plot_name.lower():  # 使用strip去除可能的逗号后，检查键是否在字符串a中
+                if key.strip(',') in plot_name.lower():  
                     found = True
-                    self.flag = flags_map[key]  # 假设self.flag需要被设置为找到的第一个匹配项对应的字典值
-                    break  # 找到第一个匹配项后即退出循环
-        st.text(self.flag)
+                    self.flag = flags_map[key]  
+                    break  
         
 
 
@@ -247,7 +246,6 @@ class plotly_go():
                     colorbar=dict(title='Label Range'),  # 添加颜色条
                 ),
             )
-            st.text("we are plotting pca")
         elif violine != 'False':
             trace = go.Violin(x0=str(file_name).split('.')[0], y=y_data, line=dict(color='black'), fillcolor=colour, name=str(file_name).split('.')[0], box_visible=True, meanline_visible=True, opacity=0.6)            
         else:
