147892c1d1d78f66867fe72efd1d564e351997f8
Shangze <89918650+DozeDuck@users.noreply.github.com>
2024-04-09 16:27:11 +0100

    Update streamlit-gmxtoolbox.py

--- streamlit-gmxtoolbox.py
+++ streamlit-gmxtoolbox.py
@@ -1153,7 +1153,7 @@ def save_uploaded_file(uploaded_file):
 
 # 在第一栏中添加内容
 with plot:
-    st.header("Plot figures")
+    st.header("plot figures")
     # Collecting user inputs
     multi_files = st.file_uploader("Upload files", accept_multiple_files=True, type=['xvg', 'gro', 'itp', 'top', 'csv'])
     # 保存上传文件的文件名
